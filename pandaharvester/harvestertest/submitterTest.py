74d0754ffc54e26c2d5535778bb7fe2367b71bcc
tmaeno <tmaeno@bnl.gov>
2017-02-16 09:23:59 +0100

    added sequence diagrams

--- pandaharvester/harvestertest/submitterTest.py
+++ pandaharvester/harvestertest/submitterTest.py
@@ -1,22 +1,41 @@
+import os
 import sys
 
-queueName = sys.argv[1]
-
 from pandaharvester.harvestercore.queue_config_mapper import QueueConfigMapper
+from pandaharvester.harvestercore.work_spec import WorkSpec
+from pandaharvester.harvestercore.plugin_factory import PluginFactory
+from pandaharvester.harvestercore.communicator_pool import CommunicatorPool
+from pandaharvester.harvestercore.job_spec import JobSpec
 
+queueName = sys.argv[1]
 queueConfigMapper = QueueConfigMapper()
-
 queueConfig = queueConfigMapper.get_queue(queueName)
 
-import os
-from pandaharvester.harvestercore.work_spec import WorkSpec
+# communicator
+com = CommunicatorPool()
+jobs = com.get_jobs(queueConfig.queueName, 'nodeName', queueConfig.prodSourceLabel, 'computingElement', 1)
+if len(jobs) == 0:
+    print "No jobs at {0}. Please submit a task or job to PanDA beforehand".format(queueConfig.queueName)
+    sys.exit(0)
+
+jobSpec = JobSpec()
+jobSpec.convert_job_json(jobs[0])
+
+# set input file paths
+inFiles = jobSpec.get_input_file_attributes()
+for inLFN, inFile in inFiles.iteritems():
+    inFile['path'] = '{0}/{1}'.format(os.getcwd(), inLFN)
+jobSpec.set_input_file_paths(inFiles)
 
 workSpec = WorkSpec()
 workSpec.accessPoint = os.getcwd()
-
-from pandaharvester.harvestercore.plugin_factory import PluginFactory
+workSpec.mapType = WorkSpec.MT_OneToOne
 
 pluginFactory = PluginFactory()
+
+# get plugin for messenger
+messenger = pluginFactory.get_plugin(queueConfig.messenger)
+messenger.feed_jobs(workSpec, [jobSpec])
 
 # get submitter plugin
 submitterCore = pluginFactory.get_plugin(queueConfig.submitter)
