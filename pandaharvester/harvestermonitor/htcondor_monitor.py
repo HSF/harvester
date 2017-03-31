7ac6739564c2f0a295ff4f817f93f19b7e0f79ae
tmaeno <tmaeno@bnl.gov>
2017-03-31 16:01:15 +0200

    added condor plugins (by FaHui Lin)

---
+++ pandaharvester/harvestermonitor/htcondor_monitor.py
@@ -0,0 +1,94 @@
+import re
+import subprocess
+
+from pandaharvester.harvestercore import core_utils
+from pandaharvester.harvestercore.work_spec import WorkSpec
+from pandaharvester.harvestercore.plugin_base import PluginBase
+
+# logger
+baseLogger = core_utils.setup_logger()
+
+
+# monitor for HTCONDOR batch system
+class HTCondorMonitor (PluginBase):
+    # constructor
+    def __init__(self, **kwarg):
+        PluginBase.__init__(self, **kwarg)
+
+    # check workers
+    def check_workers(self, workspec_list):
+        retList = []
+        for workSpec in workspec_list:
+            # make logger
+            tmpLog = core_utils.make_logger(baseLogger, 'workerID={0}'.format(workSpec.workerID))
+            # first command
+            comStr = "condor_q -l {0}".format(workSpec.batchID)
+            # first check
+            tmpLog.debug('check with {0}'.format(comStr))
+            p = subprocess.Popen(comStr.split(),
+                                 shell=False,
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE)
+            newStatus = workSpec.status
+            # first check return code
+            stdOut, stdErr = p.communicate()
+            retCode = p.returncode
+            tmpLog.debug('retCode={0}'.format(retCode))
+            errStr = ''
+            if retCode == 0:
+                # first parse
+                for tmpLine in stdOut.split('\n'):
+                    tmpMatch = re.search('JobStatus = ([^ ]+)', tmpLine)
+                    if tmpMatch is not None:
+                        batchStatus = tmpMatch.group(1)
+                        if batchStatus in ['2', '5', '6', '7', '3', '4']:
+                            # 2 running, 5 held, 6 transferring output, 7 suspended
+                            newStatus = WorkSpec.ST_running
+                        elif batchStatus in ['1']:
+                            # 1 idle
+                            newStatus = WorkSpec.ST_submitted
+                        else:
+                            newStatus = WorkSpec.ST_failed
+                        tmpLog.debug('batchStatus {0} -> workerStatus {1}'.format(batchStatus,
+                                                                                  newStatus))
+                        break
+                    else:
+                        # nothing in condor_q. Try condor_history
+                        # second command
+                        comStr = "condor_history -l {0}".format(workSpec.batchID)
+                        # second check
+                        tmpLog.debug('check with {0}'.format(comStr))
+                        p2 = subprocess.Popen(comStr.split(),
+                                              shell=False,
+                                              stdout=subprocess.PIPE,
+                                              stderr=subprocess.PIPE)
+                        # second check return code
+                        stdOut, stdErr = p2.communicate()
+                        retCode = p2.returncode
+                        tmpLog.debug('retCode={0}'.format(retCode))
+                        if retCode == 0:
+                            # second parse
+                            for tmpLine in stdOut.split('\n'):
+                                tmpMatch = re.search('JobStatus = ([^ ]+)', tmpLine)
+                                if tmpMatch is not None:
+                                    batchStatus = tmpMatch.group(1)
+                                    if batchStatus in ['4']:
+                                        # 4 completed
+                                        newStatus = WorkSpec.ST_finished
+                                    elif batchStatus in ['3']:
+                                        # 3 removed
+                                        newStatus = WorkSpec.ST_cancelled
+                                    else:
+                                        newStatus = WorkSpec.ST_failed
+                                    tmpLog.debug('batchStatus {0} -> workerStatus {1}'.format(batchStatus,
+                                                                                              newStatus))
+                                    break
+                            retList.append((newStatus, errStr))
+
+                retList.append((newStatus, errStr))
+            else:
+                # failed
+                errStr = stdOut + ' ' + stdErr
+                tmpLog.error(errStr)
+                retList.append((newStatus, errStr))
+        return True, retList
---
+++ pandaharvester/harvestersubmitter/htcondor_submitter.py
@@ -0,0 +1,65 @@
+import tempfile
+import subprocess
+
+from pandaharvester.harvestercore import core_utils
+from pandaharvester.harvestercore.plugin_base import PluginBase
+
+# logger
+baseLogger = core_utils.setup_logger()
+
+
+# submitter for HTCONDOR batch system
+class HTCondorSubmitter(PluginBase):
+    # constructor
+    def __init__(self, **kwarg):
+        PluginBase.__init__(self, **kwarg)
+        # template for batch script
+        tmpFile = open(self.templateFile)
+        self.template = tmpFile.read()
+        tmpFile.close()
+
+    # submit workers
+    def submit_workers(self, workspec_list):
+        retList = []
+        retStrList = []
+        for workSpec in workspec_list:
+            # make logger
+            tmpLog = core_utils.make_logger(baseLogger, 'workerID={0}'.format(workSpec.workerID))
+            # set nCore
+            workSpec.nCore = self.nCore
+            # make batch script
+            batchFile = self.make_batch_script(workSpec)
+            # command
+            comStr = "condor_submit {0}".format(batchFile)
+            # submit
+            tmpLog.debug('submit with {0}'.format(batchFile))
+            p = subprocess.Popen(comStr.split(),
+                                 shell=False,
+                                 stdout=subprocess.PIPE,
+                                 stderr=subprocess.PIPE)
+            # check return code
+            stdOut, stdErr = p.communicate()
+            retCode = p.returncode
+            tmpLog.debug('retCode={0}'.format(retCode))
+            if retCode == 0:
+                # extract batchID
+                workSpec.batchID = stdOut.split()[-1]
+                tmpLog.debug('batchID={0}'.format(workSpec.batchID))
+                tmpRetVal = (True, '')
+            else:
+                # failed
+                errStr = stdOut + ' ' + stdErr
+                tmpLog.error(errStr)
+                tmpRetVal = (False, errStr)
+            retList.append(tmpRetVal)
+        return retList
+
+    # make batch script
+    def make_batch_script(self, workspec):
+        tmpFile = tempfile.NamedTemporaryFile(delete=False, suffix='_submit.sdf', dir=workspec.get_access_point())
+        tmpFile.write(self.template.format(nCorePerNode=self.nCorePerNode,
+                                           nNode=workspec.nCore / self.nCorePerNode,
+                                           accessPoint=workspec.accessPoint)
+                      )
+        tmpFile.close()
+        return tmpFile.name
