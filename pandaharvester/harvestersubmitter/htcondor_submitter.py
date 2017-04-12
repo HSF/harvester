97c7a9dedf11853ce3f33f8db91fc10e0476b554
fahui <fahui.lin@twgrid.org>
2017-04-12 18:33:54 +0800

    Add dynamic resource of condor submitter and add condor SDFs.

--- pandaharvester/harvestersubmitter/htcondor_submitter.py
+++ pandaharvester/harvestersubmitter/htcondor_submitter.py
@@ -27,7 +27,7 @@ class HTCondorSubmitter(PluginBase):
             tmpLog = core_utils.make_logger(baseLogger, 'workerID={0}'.format(workSpec.workerID))
 
             ## Aggregate resources requirement from jobs
-            (agg_nCore, agg_RAM, agg_disk, max_walltime) = (0, 0, 0, 0)
+            # (agg_nCore, agg_RAM, agg_disk, max_walltime) = (0, 0, 0, 0)
             if workSpec.get_jobspec_list():
                 for jobSpec in workSpec.get_jobspec_list():
                     tmpLog.debug('PandaID={0}, nCore={1}, RAM={2}, disk={3}, wallTime={4}'.format(
@@ -37,16 +37,17 @@ class HTCondorSubmitter(PluginBase):
                                                                 jobSpec.jobParams['maxDiskCount'],
                                                                 jobSpec.jobParams['maxWalltime'],
                                                                 ) )
-                    agg_nCore += jobSpec.jobParams['coreCount']
-                    agg_RAM += jobSpec.jobParams['minRamCount']
-                    agg_disk += jobSpec.jobParams['maxDiskCount']
-                    max_walltime = max(max_walltime, jobSpec.jobParams['maxWalltime'])
+                    # agg_nCore += jobSpec.jobParams['coreCount']
+                    # agg_RAM += jobSpec.jobParams['minRamCount']
+                    # agg_disk += jobSpec.jobParams['maxDiskCount']
+                    # max_walltime = max(max_walltime, jobSpec.jobParams['maxWalltime'])
 
             ## Set overall resource requirement on this worker. Set lower bound to be 1 for each requirement
-            workSpec.nCore = max(agg_nCore, 1)
-            workSpec.minRamCount = max(agg_RAM, 1)
-            workSpec.maxDiskCount = max(agg_disk, 1)
-            workSpec.maxWalltime = max(max_walltime, 1)
+            ## For unit test only. Now Commented.
+            # workSpec.nCore = max(agg_nCore, 1)
+            # workSpec.minRamCount = max(agg_RAM, 1)
+            # workSpec.maxDiskCount = max(agg_disk, 1)
+            # workSpec.maxWalltime = max(max_walltime, 1)
 
             # make batch script
             batchFile = self.make_batch_script(workSpec)
