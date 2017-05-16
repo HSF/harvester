321e5bd2f508ecb7cf05f9f9d68a9faee45a143f
tmaeno <tmaeno@bnl.gov>
2017-05-16 14:46:06 +0200

    added Sweeper.sweep_worker

--- pandaharvester/harvestersweeper/dummy_sweeper.py
+++ pandaharvester/harvestersweeper/dummy_sweeper.py
@@ -7,10 +7,20 @@ class DummySweeper(PluginBase):
     def __init__(self, **kwarg):
         PluginBase.__init__(self, **kwarg)
 
-    # kill worker
+    # kill a worker
     def kill_worker(self, workspec):
-        """Kill a worker. If that is done synchronously in trigger_preparation
-        this method should always return True.
+        """Kill a worker in a scheduling system like batch systems and computing elements.
+
+        :param workspec: worker specification
+        :type workspec: WorkSpec
+        :return: A tuple of return code (True for success, False otherwise) and error dialog
+        :rtype: (bool, string)
+        """
+        return True, ''
+
+    # cleanup for a worker
+    def sweep_worker(self, workspec):
+        """Perform cleanup procedures for a worker, such as deletion of work directory.
 
         :param workspec: worker specification
         :type workspec: WorkSpec
