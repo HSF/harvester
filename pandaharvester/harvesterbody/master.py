d1d2386d159fd87015577e7c93da15844713ec20
tmaeno <tmaeno@bnl.gov>
2017-06-21 10:15:30 +0200

    added jobSpecFileFormat to shared_file_msgr added heartbeat to command_mgr

--- pandaharvester/harvesterbody/master.py
+++ pandaharvester/harvesterbody/master.py
@@ -28,8 +28,6 @@ class Master:
 
     # main loop
     def start(self):
-        # send first heartbeat
-        self.communicatorPool.is_alive({'startTime': datetime.datetime.utcnow()})
         # thread list
         thrList = []
         # Command manager
