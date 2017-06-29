a49ce496a5857c5feda33a2ba9e5fa8fc39ecaf5
Tadashi Maeno <tmaeno@cern.ch>
2017-06-29 09:04:31 +0200

    Update TestCommunication.py
    cleanup

--- pandaharvester/harvestertest/TestCommunication.py
+++ pandaharvester/harvestertest/TestCommunication.py
@@ -1,16 +1,7 @@
-import os
 import sys
 import logging
-import datetime
-from pandaharvester.harvesterconfig import harvester_config
-from pandaharvester.harvestercore.db_proxy_pool import DBProxyPool as DBProxy
-from pandaharvester.harvestercore.job_spec import JobSpec
-from pandaharvester.harvestercore.queue_config_mapper import QueueConfigMapper
+
 from pandaharvester.harvestercore.communicator_pool import CommunicatorPool
-try:
-    os.remove(harvester_config.db.database_filename)
-except:
-    pass
 
 for loggerName, loggerObj in logging.Logger.manager.loggerDict.iteritems():
     if loggerName.startswith('panda.log'):
@@ -22,13 +13,6 @@ for loggerName, loggerObj in logging.Logger.manager.loggerDict.iteritems():
         stdoutHandler.setFormatter(loggerObj.handlers[0].formatter)
         loggerObj.addHandler(stdoutHandler)
 
-queueConfigMapper = QueueConfigMapper()
-
-proxy = DBProxy()
-proxy.make_tables(queueConfigMapper)
-
-
 a = CommunicatorPool()
 return_object = a.check_panda()
 print return_object
-
