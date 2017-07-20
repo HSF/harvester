0b3cc817427291db019b4357553806e54500c671
tmaeno <tmaeno@bnl.gov>
2017-07-20 15:05:51 +0200

    added process lock to prevent multiple processes taking the same action for some agents

--- pandaharvester/harvesterbody/agent_base.py
+++ pandaharvester/harvesterbody/agent_base.py
@@ -1,3 +1,4 @@
+import os
 import threading
 from pandaharvester.harvestercore import core_utils
 
@@ -10,15 +11,18 @@ class AgentBase(threading.Thread):
         threading.Thread.__init__(self)
         self.singleMode = single_mode
         self.stopEvent = None
-
+        self.os_pid = os.getpid()
 
     # set stop event
     def set_stop_event(self, stop_event):
         self.stopEvent = stop_event
 
-
     # check if going to be terminated
-    def terminated(self, wait_interval):
+    def terminated(self, wait_interval, randomize=True):
         if self.singleMode:
             return True
-        return core_utils.sleep(wait_interval, self.stopEvent)
+        return core_utils.sleep(wait_interval, self.stopEvent, randomize)
+
+    # get process identifier
+    def get_pid(self):
+        return '{0}-{1}'.format(self.os_pid, self.ident)
