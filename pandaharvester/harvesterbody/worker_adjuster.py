00f4cc6e055ea46c163b56f73b5f2863ec6c8566
fbarreir <fh.barreiro@gmail.com>
2019-02-11 10:29:46 +0100

    job_stats: corrected debug message

--- pandaharvester/harvesterbody/worker_adjuster.py
+++ pandaharvester/harvesterbody/worker_adjuster.py
@@ -146,9 +146,10 @@ class WorkerAdjuster(object):
                             # limit the queue to the number of activated jobs to avoid empty pilots
                             try:
                                 n_activated = job_stats[queueName]['activated']
+                                queue_limit = maxQueuedWorkers
                                 maxQueuedWorkers = min(n_activated, maxQueuedWorkers)
                                 tmpLog.debug('limiting maxQueuedWorkers to min(n_activated={0}, queue_limit={1})'.
-                                             format(n_activated, maxQueuedWorkers))
+                                             format(n_activated, queue_limit))
                             except KeyError:
                                 maxQueuedWorkers = 1
 
