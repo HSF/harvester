8388ad5636aed34e08ed2d7de5a02a8136d05767
fahui <fahui.lin@cern.ch>
2019-04-12 18:58:10 +0800

    release v0.1.0

--- pandaharvester/commit_timestamp.py
+++ pandaharvester/commit_timestamp.py
@@ -1,1 +1,1 @@
-timestamp = "05-04-2019 08:04:40 on contrib_cern (by fahui)"
+timestamp = "12-04-2019 10:58:10 on release (by fahui)"
--- pandaharvester/panda_pkg_info.py
+++ pandaharvester/panda_pkg_info.py
@@ -1,1 +1,1 @@
-release_version = "0.1.0-rc"
+release_version = "0.1.0"
