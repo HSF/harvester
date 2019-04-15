1a6890ab7f230a99419c565e37816cfddc45fc2f
fahui <fahui.lin@cern.ch>
2019-04-15 19:51:33 +0800

    middleware:fix

--- pandaharvester/commit_timestamp.py
+++ pandaharvester/commit_timestamp.py
@@ -1,1 +1,1 @@
-timestamp = "09-04-2019 11:48:32 on contrib_cern (by fahui)"
+timestamp = "15-04-2019 11:51:33 on contrib_cern (by fahui)"
--- pandaharvester/harvestermiddleware/rpc_herder.py
+++ pandaharvester/harvestermiddleware/rpc_herder.py
@@ -15,11 +15,11 @@ _logger = core_utils.setup_logger('rpc_herder')
 class RpcHerder(PluginBase):
 
     # decorator
-    def require_alive(self, func):
+    def require_alive(func):
         @functools.wraps(func)
-        def wrapper(*args, **kwargs):
+        def wrapper(self, *args, **kwargs):
             if self.is_connected:
-                return func(*args, **kwargs)
+                return func(self, *args, **kwargs)
             else:
                 tmpLog = core_utils.make_logger(_logger, method_name=func.__name__)
                 tmpLog.warning('instance not alive; method {0} returns None'.format(func.__name__))
@@ -30,13 +30,13 @@ class RpcHerder(PluginBase):
     def __init__(self, **kwarg):
         tmpLog = core_utils.make_logger(_logger, method_name='__init__')
         PluginBase.__init__(self, **kwarg)
-        sshUserName = getattr(self, 'sshUserName', None)
-        sshPassword = getattr(self, 'sshPassword', None)
-        privateKey = getattr(self, 'privateKey', None)
-        passPhrase = getattr(self, 'passPhrase', None)
-        jumpHost = getattr(self, 'jumpHost', None)
-        jumpPort = getattr(self, 'jumpPort', 22)
-        remotePort = getattr(self, 'remotePort', 22)
+        self.sshUserName = getattr(self, 'sshUserName', None)
+        self.sshPassword = getattr(self, 'sshPassword', None)
+        self.privateKey = getattr(self, 'privateKey', None)
+        self.passPhrase = getattr(self, 'passPhrase', None)
+        self.jumpHost = getattr(self, 'jumpHost', None)
+        self.jumpPort = getattr(self, 'jumpPort', 22)
+        self.remotePort = getattr(self, 'remotePort', 22)
         # is connected only if ssh forwarding works
         self.is_connected = False
         try:
@@ -49,18 +49,18 @@ class RpcHerder(PluginBase):
     # ssh and rpc connect
     def _get_connection(self):
         tmpLog = core_utils.make_logger(_logger, method_name='_get_connection')
-        sshTunnelPool.make_tunnel_server(self.remoteHost, remotePort, self.remoteBindPort, self.numTunnels,
-                                     ssh_username=sshUserName, ssh_password=sshPassword,
-                                     private_key=privateKey, pass_phrase=passPhrase,
-                                     jump_host=jumpHost, jump_port=jumpPort)
-        tunnelHost, tunnelPort, tunnelCore = sshTunnelPool.get_tunnel(self.remoteHost, remotePort)
+        sshTunnelPool.make_tunnel_server(self.remoteHost, self.remotePort, self.remoteBindPort, self.numTunnels,
+                                     ssh_username=self.sshUserName, ssh_password=self.sshPassword,
+                                     private_key=self.privateKey, pass_phrase=self.passPhrase,
+                                     jump_host=self.jumpHost, jump_port=self.jumpPort)
+        tunnelHost, tunnelPort, tunnelCore = sshTunnelPool.get_tunnel(self.remoteHost, self.remotePort)
         self.conn = rpyc.connect(tunnelHost, tunnelPort, config={"allow_all_attrs": True,
                                                                     "allow_setattr": True,
                                                                     "allow_delattr": True})
-        tmpLog.debug('connected successfully')
+        tmpLog.debug('connected successfully to {0}:{1}'.format(tunnelHost, tunnelPort))
 
     # submit workers
-    @RpcHerder.require_alive
+    @require_alive
     def submit_workers(self, workspec_list):
         tmpLog = core_utils.make_logger(_logger, method_name='submit_workers')
         try:
@@ -68,10 +68,12 @@ class RpcHerder(PluginBase):
         except Exception:
             core_utils.dump_error_message(tmpLog)
             ret = None
+        else:
+            tmpLog.debug('done')
         return ret
 
     # check workers
-    @RpcHerder.require_alive
+    @require_alive
     def check_workers(self, workspec_list):
         tmpLog = core_utils.make_logger(_logger, method_name='check_workers')
         try:
@@ -79,10 +81,12 @@ class RpcHerder(PluginBase):
         except Exception:
             core_utils.dump_error_message(tmpLog)
             ret = None
+        else:
+            tmpLog.debug('done')
         return ret
 
     # setup access points
-    @RpcHerder.require_alive
+    @require_alive
     def setup_access_points(self, workspec_list):
         tmpLog = core_utils.make_logger(_logger, method_name='setup_access_points')
         try:
@@ -90,10 +94,12 @@ class RpcHerder(PluginBase):
         except Exception:
             core_utils.dump_error_message(tmpLog)
             ret = None
+        else:
+            tmpLog.debug('done')
         return ret
 
     # feed jobs
-    @RpcHerder.require_alive
+    @require_alive
     def feed_jobs(self, workspec, jobspec_list):
         tmpLog = core_utils.make_logger(_logger, method_name='feed_jobs')
         try:
@@ -101,10 +107,12 @@ class RpcHerder(PluginBase):
         except Exception:
             core_utils.dump_error_message(tmpLog)
             ret = None
+        else:
+            tmpLog.debug('done')
         return ret
 
     # request job
-    @RpcHerder.require_alive
+    @require_alive
     def job_requested(self, workspec):
         tmpLog = core_utils.make_logger(_logger, method_name='job_requested')
         try:
@@ -112,10 +120,12 @@ class RpcHerder(PluginBase):
         except Exception:
             core_utils.dump_error_message(tmpLog)
             ret = None
+        else:
+            tmpLog.debug('done')
         return ret
 
     # request kill
-    @RpcHerder.require_alive
+    @require_alive
     def kill_requested(self, workspec):
         tmpLog = core_utils.make_logger(_logger, method_name='kill_requested')
         try:
@@ -123,10 +133,12 @@ class RpcHerder(PluginBase):
         except Exception:
             core_utils.dump_error_message(tmpLog)
             ret = None
+        else:
+            tmpLog.debug('done')
         return ret
 
     # is alive
-    @RpcHerder.require_alive
+    @require_alive
     def is_alive(self, workspec, worker_heartbeat_limit):
         tmpLog = core_utils.make_logger(_logger, method_name='is_alive')
         try:
@@ -134,10 +146,12 @@ class RpcHerder(PluginBase):
         except Exception:
             core_utils.dump_error_message(tmpLog)
             ret = None
+        else:
+            tmpLog.debug('done')
         return ret
 
     # get work attributes
-    @RpcHerder.require_alive
+    @require_alive
     def get_work_attributes(self, workspec):
         tmpLog = core_utils.make_logger(_logger, method_name='get_work_attributes')
         try:
@@ -145,10 +159,12 @@ class RpcHerder(PluginBase):
         except Exception:
             core_utils.dump_error_message(tmpLog)
             ret = None
+        else:
+            tmpLog.debug('done')
         return ret
 
     # get output files
-    @RpcHerder.require_alive
+    @require_alive
     def get_files_to_stage_out(self, workspec):
         tmpLog = core_utils.make_logger(_logger, method_name='get_files_to_stage_out')
         try:
@@ -156,10 +172,12 @@ class RpcHerder(PluginBase):
         except Exception:
             core_utils.dump_error_message(tmpLog)
             ret = None
+        else:
+            tmpLog.debug('done')
         return ret
 
     # get events
-    @RpcHerder.require_alive
+    @require_alive
     def events_to_update(self, workspec):
         tmpLog = core_utils.make_logger(_logger, method_name='events_to_update')
         try:
@@ -167,10 +185,12 @@ class RpcHerder(PluginBase):
         except Exception:
             core_utils.dump_error_message(tmpLog)
             ret = None
+        else:
+            tmpLog.debug('done')
         return ret
 
     # request events
-    @RpcHerder.require_alive
+    @require_alive
     def events_requested(self, workspec):
         tmpLog = core_utils.make_logger(_logger, method_name='events_requested')
         try:
@@ -178,10 +198,12 @@ class RpcHerder(PluginBase):
         except Exception:
             core_utils.dump_error_message(tmpLog)
             ret = None
+        else:
+            tmpLog.debug('done')
         return ret
 
     # get PandaIDs
-    @RpcHerder.require_alive
+    @require_alive
     def get_panda_ids(self, workspec):
         tmpLog = core_utils.make_logger(_logger, method_name='get_panda_ids')
         try:
@@ -189,10 +211,12 @@ class RpcHerder(PluginBase):
         except Exception:
             core_utils.dump_error_message(tmpLog)
             ret = None
+        else:
+            tmpLog.debug('done')
         return ret
 
     # post processing
-    @RpcHerder.require_alive
+    @require_alive
     def post_processing(self, workspec, jobspec_list, map_type):
         tmpLog = core_utils.make_logger(_logger, method_name='post_processing')
         try:
@@ -200,10 +224,12 @@ class RpcHerder(PluginBase):
         except Exception:
             core_utils.dump_error_message(tmpLog)
             ret = None
+        else:
+            tmpLog.debug('done')
         return ret
 
     # send ACK
-    @RpcHerder.require_alive
+    @require_alive
     def acknowledge_events_files(self, workspec):
         tmpLog = core_utils.make_logger(_logger, method_name='acknowledge_events_files')
         try:
@@ -211,4 +237,6 @@ class RpcHerder(PluginBase):
         except Exception:
             core_utils.dump_error_message(tmpLog)
             ret = None
+        else:
+            tmpLog.debug('done')
         return ret
