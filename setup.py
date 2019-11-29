8e2f615e603c9dfa92b326a0cf12208149c7dbc1
fbarreir <fh.barreiro@gmail.com>
2019-11-29 13:59:51 +0100

    k8s: added extras_require for optional dependencies

--- setup.py
+++ setup.py
@@ -39,6 +39,10 @@ setup(
                       'psutil >= 5.4.8',
                       'scandir; python_version < "3.5"'
                       ],
+    # optional pip dependencies
+    extras_require={
+        'kubernetes':  ['kubernetes', 'pyyaml']
+    },
     data_files=[
         # config and cron files
         ('etc/panda', ['templates/panda_harvester.cfg.rpmnew.template',
