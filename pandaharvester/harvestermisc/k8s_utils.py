3519ac891d6c6c9d00155eb8d34d7a30df6da179
fbarreir <fh.barreiro@gmail.com>
2020-04-14 21:14:31 +0200

    k8s: PUSH mode

--- pandaharvester/harvestermisc/k8s_utils.py
+++ pandaharvester/harvestermisc/k8s_utils.py
@@ -16,6 +16,9 @@ from pandaharvester.harvestercore import core_utils
 
 base_logger = core_utils.setup_logger('k8s_utils')
 
+CONFIG_DIR = '/scratch/jobconfig'
+DEF_COMMAND = ["/usr/bin/bash"]
+DEF_ARGS = ["-c", "cd; wget https://raw.githubusercontent.com/HSF/harvester/k8s_analysis/pandaharvester/harvestercloud/pilots_starter.py; chmod 755 pilots_starter.py; ./pilots_starter.py || true"]
 
 class k8s_Client(object):
 
@@ -34,29 +37,60 @@ class k8s_Client(object):
 
         return yaml_content
 
-    def create_job_from_yaml(self, yaml_content, work_spec, cert, cert_in_secret=True, cpuadjustratio=100,
-                             memoryadjustratio=100):
+    def create_job_from_yaml(self, yaml_content, work_spec, container_image, cert, cert_in_secret=True,
+                             cpu_adjust_ratio=100, memory_adjust_ratio=100, executable=[], args=[]):
 
         tmp_log = core_utils.make_logger(base_logger, method_name='create_job_from_yaml')
 
+        # consider PULL mode as default, unless specified
+        submit_mode = 'PULL'
+
+        # create the configmap in push mode
+        worker_id = None
+        if work_spec.mapType != 'NoJob':
+            submit_mode = 'PUSH'
+            worker_id = str(work_spec.workerID)
+            res = self.create_configmap(work_spec)
+            if not res:  # if the configmap creation failed, don't submit a job because the pod creation will hang
+                return res, 'Failed to create a configmap'
+
+        # retrieve panda queue information
         panda_queues_dict = PandaQueuesDict()
         queue_name = panda_queues_dict.get_panda_queue_name(work_spec.computingSite)
 
+        # set the worker name
         yaml_content['metadata']['name'] = yaml_content['metadata']['name'] + "-" + str(work_spec.workerID)
 
+        # set the resource type and other metadata to filter the pods
         yaml_content['spec']['template'].setdefault('metadata', {})
-        yaml_content['spec']['template']['metadata'].update({
-            'labels': {'resourceType': str(work_spec.resourceType)}})
+        yaml_content['spec']['template']['metadata'].update({'labels':
+                                                                 {'resourceType': str(work_spec.resourceType)}
+                                                             })
 
+        # fill the container details. we can only handle one container (take the first, delete the rest)
         yaml_containers = yaml_content['spec']['template']['spec']['containers']
         del (yaml_containers[1:len(yaml_containers)])
 
         container_env = yaml_containers[0]
 
         container_env.setdefault('resources', {})
+        # set the container image
+        if 'image' not in container_env:
+            container_env['image'] = container_image
+
+        # if there is no user defined executable, run the default executable
+        if not executable:
+            executable = DEF_COMMAND
+            args = DEF_ARGS
+
+        if 'command' not in container_env:
+            container_env['command'] = executable
+            container_env['args'] = args
 
         # set the resources (CPU and memory) we need for the container
         # note that predefined values in the yaml template will NOT be overwritten
+        # Be familiar with QoS classes: https://kubernetes.io/docs/tasks/configure-pod-container/quality-service-pod
+        # The CPU & memory settings will affect the QoS for the pod
         container_env.setdefault('resources', {})
         if work_spec.nCore > 0:
 
@@ -67,20 +101,26 @@ class k8s_Client(object):
             # CPU requests
             container_env['resources'].setdefault('requests', {})
             if 'cpu' not in container_env['resources']['requests']:
-                container_env['resources']['requests']['cpu'] = str(work_spec.nCore * cpuadjustratio / 100.0)
+                container_env['resources']['requests']['cpu'] = str(work_spec.nCore * cpu_adjust_ratio / 100.0)
 
         if work_spec.minRamCount > 4:  # K8S minimum memory limit = 4 MB
             # memory limits
-            container_env['resources'].setdefault('limits', {})
-            if 'memory' not in container_env['resources']['limits']:
-                container_env['resources']['limits']['memory'] = str(work_spec.minRamCount) + 'M'
+            # container_env['resources'].setdefault('limits', {})
+            # if 'memory' not in container_env['resources']['limits']:
+            #     container_env['resources']['limits']['memory'] = str(work_spec.minRamCount) + 'M'
             # memory requests
             container_env['resources'].setdefault('requests', {})
             if 'memory' not in container_env['resources']['requests']:
                 container_env['resources']['requests']['memory'] = str(
-                    work_spec.minRamCount * memoryadjustratio / 100.0) + 'M'
+                    work_spec.minRamCount * memory_adjust_ratio / 100.0) + 'M'
 
         container_env.setdefault('env', [])
+        # try to retrieve the stdout log file name
+        try:
+            log_file_name = work_spec.workAttributes['stdout']
+        except (KeyError, AttributeError):
+            tmp_log.debug('work_spec does not have workAttributes field: {0}'.format(work_spec))
+            log_file_name = ''
 
         container_env['env'].extend([
             {'name': 'computingSite', 'value': work_spec.computingSite},
@@ -91,18 +131,30 @@ class k8s_Client(object):
             {'name': 'workerID', 'value': str(work_spec.workerID)},
             {'name': 'logs_frontend_w', 'value': harvester_config.pandacon.pandaCacheURL_W},
             {'name': 'logs_frontend_r', 'value': harvester_config.pandacon.pandaCacheURL_R},
+            {'name': 'stdout_name', 'value': log_file_name},
             {'name': 'PANDA_JSID', 'value': 'harvester-' + harvester_config.master.harvester_id},
             {'name': 'HARVESTER_WORKER_ID', 'value': str(work_spec.workerID)},
-            {'name': 'HARVESTER_ID', 'value': harvester_config.master.harvester_id}
+            {'name': 'HARVESTER_ID', 'value': harvester_config.master.harvester_id},
+            {'name': 'submit_mode', 'value': submit_mode}
         ])
 
+        # in push mode, add the configmap as a volume to the pod
+        if submit_mode == 'PUSH' and worker_id:
+            yaml_content['spec']['template']['spec'].setdefault('volumes', [])
+            yaml_volumes = yaml_content['spec']['template']['spec']['volumes']
+            yaml_volumes.append({'name': 'job-config', 'configMap': {'name': worker_id}})
+            # mount the volume to the filesystem
+            container_env.setdefault('volumeMounts', [])
+            container_env['volumeMounts'].append({'name': 'job-config', 'mountPath': CONFIG_DIR})
+
+        # set the affinity
         if 'affinity' not in yaml_content['spec']['template']['spec']:
             yaml_content = self.set_affinity(yaml_content)
 
         # tmp_log.debug('creating job {0}'.format(yaml_content))
 
         rsp = self.batchv1.create_namespaced_job(body=yaml_content, namespace=self.namespace)
-        return rsp
+        return rsp, yaml_content
 
     def get_pods_info(self):
         pods_list = list()
@@ -110,15 +162,16 @@ class k8s_Client(object):
         ret = self.corev1.list_namespaced_pod(namespace=self.namespace)
 
         for i in ret.items:
-            pod_info = {}
-            pod_info['name'] = i.metadata.name
-            pod_info['start_time'] = i.status.start_time.replace(
-                tzinfo=None) if i.status.start_time else i.status.start_time
-            pod_info['status'] = i.status.phase
-            pod_info['status_conditions'] = i.status.conditions
-            pod_info['job_name'] = i.metadata.labels[
-                'job-name'] if i.metadata.labels and 'job-name' in i.metadata.labels else None
-            pod_info['containers_state'] = []
+            pod_info = {
+                'name': i.metadata.name,
+                'start_time': i.status.start_time.replace(tzinfo=None) if i.status.start_time else i.status.start_time,
+                'status': i.status.phase,
+                'status_conditions': i.status.conditions,
+                'job_name': i.metadata.labels['job-name'] if i.metadata.labels and 'job-name' in i.metadata.labels else None,
+                'containers_state': []
+            }
+
+            # derive worker status with container state, e.g.OOMKilled
             if i.status.container_statuses:
                 for cs in i.status.container_statuses:
                     if cs.state:
@@ -139,9 +192,12 @@ class k8s_Client(object):
         ret = self.batchv1.list_namespaced_job(namespace=self.namespace, field_selector=field_selector)
 
         for i in ret.items:
-            job_info = {'name': i.metadata.name, 'status': i.status.conditions[0].type,
-                        'status_reason': i.status.conditions[0].reason,
-                        'status_message': i.status.conditions[0].message}
+            job_info = {
+                'name': i.metadata.name,
+                'status': i.status.conditions[0].type,
+                'status_reason': i.status.conditions[0].reason,
+                'status_message': i.status.conditions[0].message
+            }
             jobs_list.append(job_info)
         return jobs_list
 
@@ -165,6 +221,10 @@ class k8s_Client(object):
     def delete_job(self, job_name):
         self.batchv1.delete_namespaced_job(name=job_name, namespace=self.namespace, body=self.deletev1,
                                            grace_period_seconds=0)
+
+    def delete_config_map(self, config_map_name):
+        self.corev1.delete_namespaced_config_map(name=config_map_name, namespace=self.namespace, body=self.deletev1,
+                                                  grace_period_seconds=0)
 
     def set_proxy(self, proxy_path):
         with open(proxy_path) as f:
@@ -206,18 +266,54 @@ class k8s_Client(object):
 
         metadata = {'name': secret_name, 'namespace': self.namespace}
         data = {}
-        for file in file_list:
-            filename = os.path.basename(file)
-            with open(file, 'rb') as f:
-                aux = f.read()
-            data[filename] = base64.b64encode(aux).decode()
+        for file_name in file_list:
+            filename = os.path.basename(file_name)
+            with open(file_name, 'rb') as f:
+                content = f.read()
+            data[filename] = base64.b64encode(content).decode()
         body = client.V1Secret(data=data, metadata=metadata)
         try:
             rsp = self.corev1.patch_namespaced_secret(name=secret_name, body=body, namespace=self.namespace)
         except ApiException as e:
-            tmp_log.debug('Exception when patch secret: {0} . Try to create secret instead...'.format(e))
+            tmp_log.debug('Exception when patching secret: {0} . Try to create secret instead...'.format(e))
             rsp = self.corev1.create_namespaced_secret(body=body, namespace=self.namespace)
         return rsp
+
+    def create_configmap(self, work_spec):
+        # useful guide: https://matthewpalmer.net/kubernetes-app-developer/articles/ultimate-configmap-guide-kubernetes.html
+
+        tmp_log = core_utils.make_logger(base_logger, method_name='create_configmap')
+
+        try:
+            worker_id = str(work_spec.workerID)
+
+            # Get the access point. The messenger should have dropped the input files for the pilot here
+            access_point = work_spec.get_access_point()
+            pjd = 'pandaJobData.out'
+            job_data_file = os.path.join(access_point, pjd)
+            with open(job_data_file) as f:
+                job_data_contents = f.read()
+
+            pfc = 'PoolFileCatalog_H.xml'
+            pool_file_catalog_file = os.path.join(access_point, pfc)
+            with open(pool_file_catalog_file) as f:
+                pool_file_catalog_contents = f.read()
+
+            # put the job data and PFC into a dictionary
+            data = {pjd: job_data_contents, pfc: pool_file_catalog_contents}
+
+            # instantiate the configmap object
+            metadata = {'name': worker_id, 'namespace': self.namespace}
+            config_map = client.V1ConfigMap(api_version="v1", kind="ConfigMap", data=data, metadata=metadata)
+
+            # create the configmap object in K8s
+            api_response = self.corev1.create_namespaced_config_map(namespace=self.namespace, body=config_map)
+            tmp_log.debug('Created configmap for worker id: {0}'.format(worker_id))
+            return True
+
+        except (ApiException, TypeError) as e:
+            tmp_log.error('Could not create configmap with: {0}'.format(e))
+            return False
 
     def get_pod_logs(self, pod_name, previous=False):
         tmp_log = core_utils.make_logger(base_logger, method_name='get_pod_logs')
