7c5a78b8f60ef1d0dd18c621d63b8bdcd5213488
Pavlo Svirin <pavlo.svirin@cern.ch>
2020-06-30 19:12:19 +0000

    Updated jinja template

--- examples/slurm_submit_jinja.sh
+++ examples/slurm_submit_jinja.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-
 # Job Name and Files (also --job-name)
-#SBATCH -J w_{{worker.workerID}}
+#SBATCH -J w_{{workerID}}
 #Output and error (also --output, --error):
-#SBATCH -o {{worker.accessPoint}}/slurm_stdout.txt
-#SBATCH -e {{worker.accessPoint}}/slurm_stderr.txt
-
+#SBATCH -o {{workspec.accessPoint}}/slurm_stdout.txt
+#SBATCH -e {{workspec.accessPoint}}/slurm_stderr.txt
 #Initial working directory (also --chdir):
 #SBATCH -D ./
 
@@ -13,22 +11,19 @@
 #Setup of execution environment
 #SBATCH --export=NONE
 #SBATCH --get-user-env
-#SBATCH --account={% if nNode<16 %}"pr58be"{% else %} "general" {% endif %}
+#SBATCH --account="pr58be"
 #SBATCH --mcs-label=pr58be
-
 # Wall clock limit:
-#SBATCH --time={{walltime}}
-#SBATCH --partition=micro
-##SBATCH --time={{walltime}}
-##SBATCH --partition={% if nNode<16 %}"test"{% else %}"general" {% endif %}
-#SBATCH --ntasks-per-node 1
+#SBATCH --time=01:20:00
+##SBATCH --time-min=00:55:00
+#SBATCH --partition={% if workspec.jobspec_list|length<=16 %}"micro"{% else %}"general" {% endif %}
 
-####SBATCH -N {{nNode}}
-#SBATCH -N {{worker.nJobs}}
+#SBATCH --ntasks-per-node 1
+#SBATCH -N {{workspec.jobspec_list|length}}
 
 set -e 
 
-POSTMORTEM_FILE={{worker.accessPoint}}/FINISHED
+POSTMORTEM_FILE={{workspec.accessPoint}}/FINISHED
 
 function write_postmortem(){
     python -c "$(printf "import json,sys; a={'error_code': sys.argv[1], 'worker_status': 'finished' if sys.argv[1]=='0' else 'failed', 'last_command': sys.argv[2]}; print json.dump(a, open(sys.argv[3], 'w'));")" $? "$BASH_COMMAND" "$POSTMORTEM_FILE"
@@ -43,16 +38,20 @@ module load slurm_setup
 
 nTasks=$SLURM_JOB_NUM_NODES
 
+# Hack to force broken PRE pilot to source /release_setup.sh
+export ALRB_CONT_SETUPFILE=dummy
+
 export PANDA_QUEUE=LRZ-LMU_MUC
 
 export HARVESTER_DIR=$PANDA_HOME  # PANDA_HOME is defined in etc/sysconfig/panda_harvester
 # Actually not for virtualenv
 export HARVESTER_DIR=/home/r/Rodney.Walker/harvester
-export HARVESTER_WORKER_ID={{worker.workerID}}
-export HARVESTER_ACCESS_POINT={{worker.accessPoint}}
-export HARVESTER_NNODE={{nNode}}
+export HARVESTER_WORKER_ID={{workspec.workerID}}
+export HARVESTER_ACCESS_POINT={{workspec.accessPoint}}
+export HARVESTER_NNODE={{workspec.jobspec_list|length}}
 export HARVESTER_NTASKS=$nTasks
 export HARVESTER_CONTAINER_IMAGE=not_used
+
 
 #set variable to define HARVESTER running mode (mapType) for this worker
 #  possible choices OneToOne, OneToMany, ManyToOne
@@ -60,8 +59,7 @@ export HARVESTER_MAPTYPE=ManyToOne
 
 # Directory containing wrapper,pilot tarball and json
 #PILOT_BOOTSTRAP_DIR=/hppfs/work/pr58be/ri32buz3/pilot
-#PILOT_BOOTSTRAP_DIR=/hppfs/work/pr58be/ri32buz3/harvester/cache
-PILOT_BOOTSTRAP_DIR={{submitter.pilotBootstrapDir}}
+PILOT_BOOTSTRAP_DIR=/hppfs/work/pr58be/ri32buz3/harvester/cache
 
 export pilot_wrapper_file=$PILOT_BOOTSTRAP_DIR/runpilot2-wrapper.sh
 export pilot_tar_file=$PILOT_BOOTSTRAP_DIR/pilot2.tar.gz
@@ -73,10 +71,6 @@ export create_singularity_wrapper_file=$PILOT_BOOTSTRAP_DIR/create_singularity_s
 
 # get the PanDA Job IDs from worker_pandaids.json 
 PANDA_JOB_IDS=( "$(python -c "import json;pids = json.load(open('worker_pandaids.json'));print(' '.join(str(x) for x in pids))")" )
-PANDA_JOB_INFO=( "$(python -c "import json;pids = json.load(open('worker_pandaids.json'));print(' '.join(str(x)+';'+k['container_name'] if 'container_name' in k else '' for x,k in pids))")" )
-
-
-# environment variable initialization finished
 
 
 # record the various HARVESTER envars
@@ -89,8 +83,8 @@ echo [$SECONDS] "Harvester accessPoint - "$HARVESTER_ACCESS_POINT
 echo [$SECONDS] "pilot wrapper file - "$pilot_wrapper_file
 echo [$SECONDS] "Pilot tar file - "$pilot_tar_file
 echo [$SECONDS] "Container image - "$HARVESTER_CONTAINER_IMAGE
-echo [$SECONDS] "Command to setup release in container - "$HARVESTER_CONTAINER_RELEASE_SETUP_FILE
-echo [$SECONDS] "PANDA_JOB_IDS: $PANDA_JOB_IDS"
+echo [$SECONDS] "command to setup release in container - "$HARVESTER_CONTAINER_RELEASE_SETUP_FILE
+echo [$SECONDS] PANDA_JOB_IDS: $PANDA_JOB_IDS
 echo [$SECONDS] "Number of Nodes to use - "$HARVESTER_NNODE
 echo [$SECONDS] "Number of tasks for srun - "$HARVESTER_NTASKS 
 echo [$SECONDS] "ATHENA_PROC_NUMBER - "$ATHENA_PROC_NUMBER
@@ -114,12 +108,7 @@ echo [$SECONDS] ATLAS_LOCAL_ROOT_BASE = $ATLAS_LOCAL_ROOT_BASE
 
 # Loop over PanDA ID's
 
-#for PANDA_JOB_ID in $PANDA_JOB_IDS;  do
-
-for PANDA_JOB in $PANDA_JOB_INFO;  do
-    IFS=';' read -a TMP_JOB_INFO <<< "${PANDA_JOB}"
-    PANDA_JOB_ID=${TMP_JOB_INFO[0]}
-    HARVESTER_CONTAINER_IMAGE=${TMP_JOB_INFO[1]}
+for PANDA_JOB_ID in $PANDA_JOB_IDS;  do
 
     echo [$SECONDS] "PanDA Job ID - "$PANDA_JOB_ID
     
@@ -171,7 +160,11 @@ for PANDA_JOB in $PANDA_JOB_INFO;  do
     cat ./singularity_wrapper_file.sh
     echo 
 
-    # start execution
+
+
+
+    export IMAGE_BASE=/hppfs/work/pr58be/ri32buz3/images/
+   # export IMAGE_BASE=/hppfs/scratch/01/ri32buz3/images/
 
     # In ManyToOne running or OnetoOne running want one launch on srun per node - per PanDAid
     if [[ "$HARVESTER_MAPTYPE" = "OneToMany" ]] ; then    #AKA Jumbo jobs
@@ -179,23 +172,16 @@ for PANDA_JOB in $PANDA_JOB_INFO;  do
         # now start things up
 	echo [$SECONDS] "Launching srun command from : " $PWD
 
-	#export IMAGE_BASE=/hppfs/work/pr58be/ri32buz3/images/
-	export IMAGE_BASE={{submitter.imageBase}}
-	# CONTAINER_IMAGE=
-        # read it from file
+	echo [$SECONDS] srun -n $nTasks  -o PandaID-$PANDA_JOB_ID-%N-%j-%t.log \
+	         singularity exec --bind "/hpcgpfs01/work/benjamin/yampl:/opt/yampl" $HARVESTER_CONTAINER_IMAGE \
+              /bin/bash ./BNL_runpilot2-wrapper.sh -s $PANDA_QUEUE -r $PANDA_QUEUE \
+	         -q $PANDA_QUEUE -j managed -i PR -t --mute --harvester SLURM_NODEID \
+	         --harvester_workflow $HARVESTER_MAPTYPE \
+	         --container -w generic --pilot-user=atlas --hpc-resource cori --use-https False \
+	         -d --harvester-submit-mode=PUSH \
+	         --harvester-workdir=$HARVESTER_WORKDIR \
+	         --allow-same-user=False --resource-type MCORE -z 
 
-	echo -n "[$SECONDS] "
-        #  srun -n $nTasks  -o PandaID-$PANDA_JOB_ID-%N-%j-%t.log \
-	#         singularity exec --bind "/hpcgpfs01/work/benjamin/yampl:/opt/yampl" $HARVESTER_CONTAINER_IMAGE \
-        #      /bin/bash ./BNL_runpilot2-wrapper.sh -s $PANDA_QUEUE -r $PANDA_QUEUE \
-	#         -q $PANDA_QUEUE -j managed -i PR -t --mute --harvester SLURM_NODEID \
-	#         --harvester_workflow $HARVESTER_MAPTYPE \
-	#         --container -w generic --pilot-user=atlas --hpc-resource cori --use-https False \
-	#         -d --harvester-submit-mode=PUSH \
-	#         --harvester-workdir=$HARVESTER_WORKDIR \
-	#         --allow-same-user=False --resource-type MCORE -z 
-
-	set -x
 	srun -n $nTasks  -o PandaID-$PANDA_JOB_ID-%N-%j-%t.log \
 	         singularity exec --bind "/hpcgpfs01/work/benjamin/yampl:/opt/yampl" $HARVESTER_CONTAINER_IMAGE \
              /bin/bash ./BNL_runpilot2-wrapper.sh -s $PANDA_QUEUE -r $PANDA_QUEUE \
@@ -205,7 +191,6 @@ for PANDA_JOB in $PANDA_JOB_INFO;  do
 	         -d --harvester-submit-mode=PUSH \
 	         --harvester-workdir=$HARVESTER_WORKDIR \
 	         --allow-same-user=False --resource-type MCORE -z &
-        set +x
     else
         # now start things up
 	echo [$SECONDS] "Hostname : " $HOSTNAME
@@ -220,4 +205,12 @@ done
 
 echo [$SECONDS] " wait command  "
 wait
+
+pwd 
+ls -l
+
+# Write a status file so squeue monitor can see worker if finshed
+# curly brackets looks like template
+#touch $HARVESTER_ACCESS_POINT/FINISHED
+
 
