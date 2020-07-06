436d6977c90fc873e13b81b26341ca6a7ac3a83d
fahui <fahui.lin@cern.ch>
2020-07-06 13:51:26 +0200

    version 0.2.4-rc

--- pandaharvester/commit_timestamp.py
+++ pandaharvester/commit_timestamp.py
@@ -1,1 +1,1 @@
-timestamp = "06-07-2020 11:31:31 on flin (by fahui)"
+timestamp = "06-07-2020 11:51:26 on flin (by fahui)"
--- pandaharvester/panda_pkg_info.py
+++ pandaharvester/panda_pkg_info.py
@@ -1,1 +1,1 @@
-release_version = "0.2.3-rc"
+release_version = "0.2.4-rc"
