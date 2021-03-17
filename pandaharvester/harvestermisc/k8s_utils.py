40cb6f3582b217a99469c80061d6a3fc9bc1e952
fbarreir <fh.barreiro@gmail.com>
2021-03-17 20:19:18 +0100

    Horovod: small changes

--- pandaharvester/harvestermisc/k8s_utils.py
+++ pandaharvester/harvestermisc/k8s_utils.py
@@ -71,7 +71,7 @@ do
     echo; 
 
     echo \"=== exec script ===\"; 
-    /bin/sh $SHARED_DIR/payload_workdir/__run_main_exec.sh >> $SHARED_DIR/payload_workdir/run_main_exec.log;
+    /bin/sh $SHARED_DIR/payload_workdir/__run_main_exec.sh | tee $SHARED_DIR/payload_workdir/run_main_exec.log;
     REAL_MAIN_RET_CODE=$?; 
     
     echo \"=== finished with ===\";
@@ -771,8 +771,8 @@ class k8s_Client(object):
         deployment_name = "{0}-{1}".format(HOROVOD_WORKER_TAG, worker_id)
 
         # TODO: decide how the memory and CPU/GPU requirements will be calculated
-        resources = client.V1ResourceRequirements(requests={"cpu": "1500m", "memory": "3500Mi"},
-                                                  limits={"cpu": "1500m", "memory": "3500Mi"})
+        resources = client.V1ResourceRequirements(requests={"cpu": "1500m", "memory": "3000Mi"},
+                                                  limits={"cpu": "1500m", "memory": "3000Mi"})
 
         container = client.V1Container(command=command, name=HOROVOD_WORKER_TAG, image=container_image,
                                        volume_mounts=[ssh_keys_mount], env=env, resources=resources)
