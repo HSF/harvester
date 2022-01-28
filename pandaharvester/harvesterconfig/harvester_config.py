4a40a0df069dc31acc9d48e0034ab1250c9abad7
tmaeno <tmaeno@bnl.gov>
2022-01-28 10:22:55 +0100

    configmap

--- pandaharvester/harvesterconfig/harvester_config.py
+++ pandaharvester/harvesterconfig/harvester_config.py
@@ -26,10 +26,22 @@ class _SectionClass:
         pass
 
 
+# load configmap
+config_map_data = {}
+if 'PANDA_HOME' in os.environ:
+    config_map_name = 'panda_harvester_configmap.json'
+    config_map_path = os.path.join(os.environ['PANDA_HOME'], 'etc/configmap', config_map_name)
+    if os.path.exists(config_map_path):
+        with open(config_map_path) as f:
+            config_map_data = json.load(f)
+
 # loop over all sections
 for tmpSection in tmpConf.sections():
     # read section
     tmpDict = getattr(tmpConf, tmpSection)
+    # load configmap
+    if tmpSection in config_map_data:
+        tmpDict.update(config_map_data[tmpSection])
     # make section class
     tmpSelf = _SectionClass()
     # update module dict
