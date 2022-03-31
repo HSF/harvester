f7c9f0087284a33c27db9a818ccca0724230ec09
fbarreir <fh.barreiro@gmail.com>
2022-03-31 17:19:30 +0200

    python3 compatibility for pilots_starter.py

--- pandaharvester/harvestercloud/pilots_starter.py
+++ pandaharvester/harvestercloud/pilots_starter.py
@@ -13,14 +13,23 @@ try:
     import subprocess32 as subprocess
 except Exception:
     import subprocess
+
+try:
+    import http.client as httplib  # for python 3
+except Exception:
+    import httplib  # for python 2
+
+try:
+    import urllib.parse as urlparse  # for python 3
+except ImportError:
+    import urlparse  # for python 2
+
 import os
 import sys
 import shutil
 import logging
-import httplib
 import mimetypes
 import ssl
-import urlparse
 import traceback
 
 WORK_DIR = '/scratch'
