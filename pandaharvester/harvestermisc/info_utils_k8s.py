1b04bccec7d50179d4c3fec97a5e73f2a998ddd1
fbarreir <fh.barreiro@gmail.com>
2022-07-14 14:47:24 +0200

    compact the code

--- pandaharvester/harvestermisc/info_utils_k8s.py
+++ pandaharvester/harvestermisc/info_utils_k8s.py
@@ -30,35 +30,11 @@ class PandaQueuesDictK8s(PandaQueuesDict):
         key_priority_class_mcore = 'k8s.scheduler.priorityClassName.mcore'
         key_priority_class_mcore_himem = 'k8s.scheduler.priorityClassName.mcore_himem'
 
-        try:
-            ret_map['priority_class'] = params[key_priority_class]
-        except KeyError:
-            # return default value
-            ret_map['priority_class'] = None
-
-        try:
-            ret_map['priority_class_score'] = params[key_priority_class_score]
-        except KeyError:
-            # return default value
-            ret_map['priority_class_score'] = None
-
-        try:
-            ret_map['priority_class_score_himem'] = params[key_priority_class_score_himem]
-        except KeyError:
-            # return default value
-            ret_map['priority_class_score_himem'] = None
-
-        try:
-            ret_map['priority_class_mcore'] = params[key_priority_class_mcore]
-        except KeyError:
-            # return default value
-            ret_map['priority_class_mcore'] = None
-
-        try:
-            ret_map['priority_class_mcore_himem'] = params[key_priority_class_mcore_himem]
-        except KeyError:
-            # return default value
-            ret_map['priority_class_mcore_himem'] = None
+        ret_map['priority_class'] = params.get(key_priority_class, None)
+        ret_map['priority_class_score'] = params.get(key_priority_class_score, None)
+        ret_map['priority_class_score_himem'] = params.get(key_priority_class_score_himem, None)
+        ret_map['priority_class_mcore'] = params.get(key_priority_class_mcore, None)
+        ret_map['priority_class_mcore_himem'] = params.get(key_priority_class_mcore_himem, None)
 
         return ret_map
 
@@ -68,65 +44,25 @@ class PandaQueuesDictK8s(PandaQueuesDict):
 
         # this is how the CPU parameters are declared in CRIC
         key_cpu_scheduling_ratio = 'k8s.resources.requests.cpu_scheduling_ratio'
-
-        try:
-            cpu_scheduling_ratio = params[key_cpu_scheduling_ratio]
-        except KeyError:
-            # return default value
-            cpu_scheduling_ratio = 90
-        ret_map['cpu_scheduling_ratio'] = cpu_scheduling_ratio
+        ret_map['cpu_scheduling_ratio'] = params.get(key_cpu_scheduling_ratio, 90)
 
         # this is how the memory parameters are declared in CRIC
         key_memory_limit = 'k8s.resources.limits.use_memory_limit'
         key_memory_limit_safety_factor = 'k8s.resources.limits.memory_limit_safety_factor'
         key_memory_limit_min_offset = 'k8s.resources.limits.memory_limit_min_offset'
 
-        try:
-            use_memory_limit = params[key_memory_limit]
-        except KeyError:
-            # return default value
-            use_memory_limit = False
-        ret_map['use_memory_limit'] = use_memory_limit
-
-        try:
-            memory_limit_safety_factor = params[key_memory_limit_safety_factor]
-        except KeyError:
-            # return default value
-            memory_limit_safety_factor = 100
-        ret_map['memory_limit_safety_factor'] = memory_limit_safety_factor
-
-        try:
-            memory_limit_min_offset = params[key_memory_limit_min_offset]  # in MiB to be consistent with minRamCount
-        except KeyError:
-            # return default value
-            memory_limit_min_offset = 0
-        ret_map['memory_limit_min_offset'] = memory_limit_min_offset
+        ret_map['use_memory_limit'] = params.get(key_memory_limit, False)
+        ret_map['memory_limit_safety_factor'] = params.get(key_memory_limit_safety_factor, 100)
+        ret_map['memory_limit_min_offset'] = params.get(key_memory_limit_min_offset, 0)  # in MiB to be consistent with minRamCount
 
         # this is how the ephemeral storage parameters are declared in CRIC
         key_ephemeral_storage = 'k8s.resources.use_ephemeral_storage_resource_specs'
         key_ephemeral_storage_resources_offset = 'k8s.resources.ephemeral_storage_offset'
         key_ephemeral_storage_limit_safety_factor = 'k8s.resources.limits.ephemeral_storage_limit_safety_factor'
 
-        try:
-            use_ephemeral_storage = params[key_ephemeral_storage]
-        except KeyError:
-            # use ephemeral storage unless explicitly disabled
-            use_ephemeral_storage = True
-        ret_map['use_ephemeral_storage'] = use_ephemeral_storage
-
-        try:
-            ephemeral_storage_limit_safety_factor = params[key_ephemeral_storage_limit_safety_factor]
-        except KeyError:
-            # return default value
-            ephemeral_storage_limit_safety_factor = 100
-        ret_map['ephemeral_storage_limit_safety_factor'] = ephemeral_storage_limit_safety_factor
-
-        try:
-            ephemeral_storage_offset = params[key_ephemeral_storage_resources_offset]
-        except KeyError:
-            # return default value
-            ephemeral_storage_offset = 0  # should come in MiB
-        ret_map['ephemeral_storage_offset'] = ephemeral_storage_offset
+        ret_map['use_ephemeral_storage'] = params.get(key_ephemeral_storage, True)  # use ephemeral storage unless explicitly disabled
+        ret_map['ephemeral_storage_limit_safety_factor'] = params.get(key_ephemeral_storage_limit_safety_factor, 100)
+        ret_map['ephemeral_storage_offset'] = params.get(key_ephemeral_storage_resources_offset, 0)  # should come in MiB
 
         return ret_map
 
@@ -158,25 +94,26 @@ class PandaQueuesDictK8s(PandaQueuesDict):
         # check if host_image defined in the associated parameter section
         key_host_image = 'k8s.host_image'
         params = self.get_harvester_params(panda_resource)
-
-        host_image = None
-        try:
-            host_image = params[key_host_image]
-        except KeyError:
-            pass
+        host_image = params.get(key_host_image, None)
 
         return host_image
 
     def get_k8s_pilot_dir(self, panda_resource):
 
-        # check if host_image defined in the associated parameter section
+        # TODO: check if it can be replaced by an existing PQ field like tmpdir or wntmpdir
+        # check if pilot_dir_mount defined in the associated parameter section
         key_pilot_dir = 'k8s.volumes.pilot_dir_mount'
         params = self.get_harvester_params(panda_resource)
-
-        pilot_dir = '/pilotdir'
-        try:
-            pilot_dir = params[key_pilot_dir]
-        except KeyError:
-            pass
+        pilot_dir = params.get(key_pilot_dir, '/pilotdir')
 
         return pilot_dir
+
+    def get_k8s_annotations(self, panda_resource):
+
+        # check if there are annotations to be specified
+        key_safe_to_evict = 'k8s.annotations.safe_to_evict'
+
+        params = self.get_harvester_params(panda_resource)
+        safe_to_evict = params.get(key_safe_to_evict, None)
+
+        return safe_to_evict
