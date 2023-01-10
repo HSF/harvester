05c4f270f9e28ba7960117e0f91af8332897b03e
fbarreir <fh.barreiro@gmail.com>
2023-01-10 16:46:45 +0100

    lancium test scripts

--- pandaharvester/harvestertest/lancium/clean.py
+++ pandaharvester/harvestertest/lancium/clean.py
@@ -1,11 +1,7 @@
 from lancium.api.Job import Job
 from lancium.errors.common import *
 
-kwargs = {'name': 'This is a test job', 'image': 'lancium/ubuntu', 'command': 'ls', 'notes': 'this is a note'}
-job = Job.create(**kwargs)
-
-job_id = job.id
-print(job_id)
+job_id = 1234
 
 #  What is the difference between terminate, delete and destroy a job
 #  Does delete also terminate a job?
