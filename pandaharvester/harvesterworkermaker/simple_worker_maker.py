d63e216f6a26a6444cc28ced83be4a7e43aea7f0
mightqxc <fahui.lin@cern.ch>
2024-03-15 16:47:54 +0100

    update worker rt with job rt

--- pandaharvester/harvesterworkermaker/simple_worker_maker.py
+++ pandaharvester/harvesterworkermaker/simple_worker_maker.py
@@ -150,6 +150,10 @@ class SimpleWorkerMaker(BaseWorkerMaker):
                     ioIntensity += jobSpec.jobParams["ioIntensity"]
                 except Exception:
                     pass
+            # update resource type
+            job_resource_type = getattr(jobspec_list[0], "resourceType", None)
+            if job_resource_type:
+                resource_type = job_resource_type
             # fill in worker attributes
             if is_ucore or (nCore > 0 and "nCore" in self.jobAttributesToUse):
                 workSpec.nCore = nCore
