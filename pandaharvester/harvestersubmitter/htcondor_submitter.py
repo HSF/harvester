d0ab37aa8f12a105b27d47ab09d7e25ea7a10069
Wen Guan <wguan.icedew@gmail.com>
2025-05-23 16:06:56 +0200

    fix n_node

--- pandaharvester/harvestersubmitter/htcondor_submitter.py
+++ pandaharvester/harvestersubmitter/htcondor_submitter.py
@@ -219,7 +219,10 @@ def make_a_jdl(
             tmpLog.debug(f"job attributes override by CRIC special_par: {attr}={str(_match.group(1))}")
     # derived job attributes
     n_core_total_factor = n_core_total * n_core_factor
-    n_node = ceil(n_core_total / n_core_per_node)
+    if not n_core_per_node or n_core_per_node < 1:
+        n_node = 1
+    else:
+        n_node = ceil(n_core_total / n_core_per_node)
     request_ram_factor = request_ram * n_core_factor
     request_ram_bytes = request_ram * 2**20
     request_ram_bytes_factor = request_ram * 2**20 * n_core_factor
@@ -667,7 +670,7 @@ class HTCondorSubmitter(PluginBase):
 
         # get override requirements from queue configured
         try:
-            n_core_per_node = self.nCorePerNode if self.nCorePerNode else n_core_per_node_from_queue
+            n_core_per_node = self.nCorePerNode if self.nCorePerNode is not None else n_core_per_node_from_queue
         except AttributeError:
             n_core_per_node = n_core_per_node_from_queue
 
