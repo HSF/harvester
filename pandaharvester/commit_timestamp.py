411252cc1a9af6648b0a0e9d4f724c05858125f4
mightqxc <fahui.lin@cern.ch>
2026-09-17 13:37:17 +0200

    v0.7.6

--- pandaharvester/commit_timestamp.py
+++ pandaharvester/commit_timestamp.py
@@ -1,1 +1,1 @@
-timestamp = "17-09-2026 11:17:12 on flin (by mightqxc)"
+timestamp = "17-09-2026 11:37:17 on flin (by mightqxc)"
--- pandaharvester/panda_pkg_info.py
+++ pandaharvester/panda_pkg_info.py
@@ -1,1 +1,1 @@
-release_version = "0.8.0-rc0"
+release_version = "0.7.6"
